ba89526409dfdac83ea4263916a4fa52f238a564
KacKoz <kacpro@onet.com.pl>
2025-06-09 22:12:40 +0200

    Delet garbage and send comments to file-sink

--- file-sink_setup.sh
+++ file-sink_setup.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+
 cd file-sink
 eval $(minikube docker-env)
 docker build -t file-sink:latest .
 docker tag file-sink:latest dev.local/file-sink:latest
-# kubectl apply -f config/file-sink.yaml
-kn service apply config/file-sink.yaml --image=dev.local/file-sink --pull-policy=Never
+kn service apply file-sink --image=dev.local/file-sink --pull-policy=Never
+cd -
 
 #test the service
-kubectl run test --image=curlimages/curl -it --rm -- /bin/sh
-curl -X POST http://file-sink.default.svc.cluster.local -H "Content-Type: application/json" -d '{"message": "duuuuupa"}'
-kubectl exec -it <file-sink-pod-name> -- cat /data/events.log
+# kubectl run test --image=curlimages/curl -it --rm -- /bin/sh
+# curl -X POST http://file-sink.default.svc.cluster.local -H "Content-Type: application/json" -d '{"message": "duuuuupa"}'
+# kubectl exec -it <file-sink-pod-name> -- cat /data/events.log
