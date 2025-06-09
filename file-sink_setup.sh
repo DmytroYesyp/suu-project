2fdc6ccc7830e06fd3580524ecb0a055357c7782
Jan Chyczynski <jasiek.ch16@gmail.commgit>
2025-06-09 20:38:51 +0200

    file-sink working

--- file-sink_setup.sh
+++ file-sink_setup.sh
@@ -1,3 +1,11 @@
 cd file-sink
+eval $(minikube docker-env)
 docker build -t file-sink:latest .
-kubectl apply -f config/file-sink.yaml
+docker tag file-sink:latest dev.local/file-sink:latest
+# kubectl apply -f config/file-sink.yaml
+kn service apply config/file-sink.yaml --image=dev.local/file-sink --pull-policy=Never
+
+#test the service
+kubectl run test --image=curlimages/curl -it --rm -- /bin/sh
+curl -X POST http://file-sink.default.svc.cluster.local -H "Content-Type: application/json" -d '{"message": "duuuuupa"}'
+kubectl exec -it <file-sink-pod-name> -- cat /data/events.log
